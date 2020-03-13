e61657f1e35334f69a9fe2a0e5ce127c9915ac9b
Spenser Short <spenscershort@gmail.com>
2020-03-13 14:27:08 -0400

    Basic Start

---
+++ BinaryxDec/BinaryxDec.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    BinaryxDec bxd;
+}
+
+class BinaryxDec {
+private:
+    int dec = 0;
+    string bin = "00000000";
+    //largest 8 bit number = 255, largest 16 bit number = 65535, largest 32 bit number 2147483647
+    void output() {//this function is used to handle outputs to the user
+
+    }
+
+    void input() {//this function is used to handle user inputs
+
+    }
+
+    void cycle() {//this is used to cause the program to repeat indefinitely until quit
+
+    }
+public:
+    BinaryxDec() {//constructor
+
+    }
+};
